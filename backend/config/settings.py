ce6f44a80dee4a517f482c7b096d8e12cc9d9a4f
Mokey2002 <edventura91@gmail.com>
2026-02-02 21:12:14 -0800

    switched to jwt instead

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,13 +49,12 @@ INSTALLED_APPS = [
     'custom_fields',
 ]
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
-    ],
+    ),
 }
 
 MIDDLEWARE = [
