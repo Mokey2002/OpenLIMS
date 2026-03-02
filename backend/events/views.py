204f6efca5946e866ecb25b87ef885138e7e1bfa
Mokey2002 <edventura91@gmail.com>
2026-03-02 14:22:22 -0800

    Added permissions to events and inventory

--- backend/events/views.py
+++ backend/events/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Event
 from .serializers import EventSerializer
+from core.permissions import IsAuthenticatedReadOnly
 
 class EventViewSet(ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticatedReadOnly]
     queryset = Event.objects.all().order_by("-timestamp")
     serializer_class = EventSerializer
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Location, Container
 from .serializers import LocationSerializer, ContainerSerializer
+from core.permissions import IsAuthenticatedReadOnlyOrTechAdminWrite, IsAdminOnly, IsAuthenticatedReadOnly
+from rest_framework.permissions import SAFE_METHODS
+
+class InventoryAdminWritePermission(IsAdminOnly):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return bool(request.user and request.user.is_authenticated)
+        return super().has_permission(request, view)
 
 class LocationViewSet(ModelViewSet):
+    permission_classes = [InventoryAdminWritePermission]
     queryset = Location.objects.all().order_by("name")
     serializer_class = LocationSerializer
 
 
 class ContainerViewSet(ModelViewSet):
+    permission_classes = [InventoryAdminWritePermission]
     queryset = Container.objects.select_related("location").all().order_by("container_id")
     serializer_class = ContainerSerializer
 
