53641b5eeb86693a5c2c54cdad99abaadd68566c
Mokey2002 <edventura91@gmail.com>
2026-05-14 15:43:11 -0700

    fixed serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -22,6 +22,16 @@ class UserCreateSerializer(serializers.ModelSerializer):
             "password",
             "role",
             "roles",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        ]
+        read_only_fields = [
+            "id",
+            "roles",
+            "is_active",
+            "is_staff",
+            "is_superuser",
         ]
 
     def create(self, validated_data):
@@ -30,12 +40,53 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
         user = User.objects.create(**validated_data)
         user.set_password(password)
+
+        if role == "admin":
+            user.is_staff = True
+
         user.save()
 
         group, _ = Group.objects.get_or_create(name=role)
         user.groups.add(group)
 
         return user
+
+    def get_roles(self, obj):
+        return list(obj.groups.values_list("name", flat=True))
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    roles = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "roles",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+        ]
+
+    def get_roles(self, obj):
+        return list(obj.groups.values_list("name", flat=True))
+
+
+class MeSerializer(serializers.ModelSerializer):
+    roles = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "roles",
+            "is_staff",
+            "is_superuser",
+        ]
 
     def get_roles(self, obj):
         return list(obj.groups.values_list("name", flat=True))
